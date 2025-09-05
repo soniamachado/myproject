6a8b06a2969388b3b59d63c34daa78364ee32166
soniamachado <soniamachado13@gmx.com>
2025-09-05 18:17:47 +0100

    variáveis primei vez

---
+++ python/variaveis.py
@@ -0,0 +1,18 @@
+#executar o seguinte código pyhton
+x=10
+y=15
+z=x+y
+print("soma de x com y=',z")
+print("tipo de dados da variável z:", type (z))
+
+""" Exercício 3
+Implementar um programa em Python com os seguintes requisitos:
+o atribuir o valor 100.78 para a variável total
+o calcular para uma outra variável o dobro do valor da variável total
+o calcular para uma outra variável o triplo do valor da variável total
+De salientar que todos os valores calculados anteriormente devem ser escritos no terminal"""
+var1=100.79
+var2=2*var1
+var3=3*var1
+
+print ('var1=', var1,'var2=',var2,'var3=',var3)
