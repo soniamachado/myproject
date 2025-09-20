33b1c815b9ac057cca6e603a82cc26775e5f8464
soniamachado <soniamachado13@gmx.com>
2025-09-20 17:45:38 +0100

    Renomear ficheiros para convenção labX_nome

--- python/renomear_e_commit.sh
+++ python/renomear_e_commit.sh
@@ -3,9 +3,9 @@
 # Renomear ficheiros conforme solicitaste
 mv variaveis.py lab2_variaveis2.py
 mv leitura_de_dados.py lab3_leitura_de_dados.py
-mv estrutura_de_controlo.py lab4_estrutura_de_controlo.py
+mv estrutura_de_control.py lab4_estrutura_de_controlo.py
 mv funcoes.py lab5_funcoes.py
-mv estrutura_de_itereativos.py lab6_estrutura_de_iterativos.py
+mv iterativos_loops.py lab6_iterativos_loops.py
 mv import.py lab7_import.py
 
 # Adicionar todas as alterações
